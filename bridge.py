3aedf9c3a1eae6650fb70f046db4eb778b2f5a38
VadymKotsiuba1808 <vadymkotsiuba@icloud.com>
2023-05-25 21:09:08 +0300

    Fix requirements and add flask bridge

---
+++ bridge.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/2_4_ghz', methods=['POST'])
+def receive_list():
+    data_list = request.get_json()
+    print('Received list: ', data_list)
+
+    return 'List received successfully'
+
+app.run()

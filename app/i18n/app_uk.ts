c513d658da8750a395f133aa24b590d771f551b2
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-10-28 23:05:26 +0200

    Added uk translated words

--- app/i18n/app_uk.ts
+++ app/i18n/app_uk.ts
@@ -1,305 +1,305 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-  <context>
+<TS version="2.1" language="uk_UA">
+<context>
     <name>ChangePwdDialog</name>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Зміна паролю</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Зміна паролю</source>
+        <translation>Зміна паролю</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>ЗМІНА ПАРОЛЮ</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>ЗМІНА ПАРОЛЮ</source>
+        <translation>ЗМІНА ПАРОЛЮ</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Введіть пароль</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Введіть пароль</source>
+        <translation>Введіть пароль</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Паролі не співпадають</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Паролі не співпадають</source>
+        <translation>Паролі не співпадають</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>error</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>error</source>
+        <translation>error</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>eye_btn</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>eye_btn</source>
+        <translation>eye_btn</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>hidden</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>hidden</source>
+        <translation>hidden</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Введіть пароль:</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Введіть пароль:</source>
+        <translation>Введіть пароль:</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Підтвердіть пароль:</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Підтвердіть пароль:</source>
+        <translation>Підтвердіть пароль:</translation>
     </message>
     <message>
-      <location filename="..\ui\change_pwd_dialog.ui" line="0" />
-      <source>Зберегти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/change_pwd_dialog.ui" line="0"/>
+        <source>Зберегти</source>
+        <translation>Зберегти</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>LoginDialog</name>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Авторизація</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Авторизація</source>
+        <translation>Авторизація</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>АВТОРИЗАЦІЯ</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>АВТОРИЗАЦІЯ</source>
+        <translation>АВТОРИЗАЦІЯ</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Оберіть вашу роль:</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Оберіть вашу роль:</source>
+        <translation>Оберіть вашу роль:</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Оператор</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Оператор</source>
+        <translation>Оператор</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Власник</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Власник</source>
+        <translation>Власник</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Пароль неправильний</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Пароль неправильний</source>
+        <translation>Пароль неправильний</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>error</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>error</source>
+        <translation>error</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Пароль:</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Пароль:</source>
+        <translation>Пароль:</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Запам'ятати мене</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Запам&apos;ятати мене</source>
+        <translation>Запам&apos;ятати мене</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Введіть пароль</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Введіть пароль</source>
+        <translation>Введіть пароль</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>eye_btn</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>eye_btn</source>
+        <translation>eye_btn</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>hidden</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>hidden</source>
+        <translation>hidden</translation>
     </message>
     <message>
-      <location filename="..\ui\login_dialog.ui" line="0" />
-      <source>Увійти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/login_dialog.ui" line="0"/>
+        <source>Увійти</source>
+        <translation>Увійти</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>SDR Drone Detector</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>SDR Drone Detector</source>
+        <translation>SDR Drone Detector</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>05.01.2022</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>05.01.2022</source>
+        <translation>05.01.2022</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <location filename="..\ui\main_window.ui" line="0" />
-      <location filename="..\ui\main_window.ui" line="0" />
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>saved</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <location filename="../ui/main_window.ui" line="0"/>
+        <location filename="../ui/main_window.ui" line="0"/>
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>saved</source>
+        <translation>saved</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>-</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>-</source>
+        <translation>-</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Radio, GHz</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Radio, GHz</source>
+        <translation>Radio, GHz</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Sound, GHz</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Sound, GHz</source>
+        <translation>Sound, GHz</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Тривога RF</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Тривога RF</source>
+        <translation>Тривога RF</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Тривога Sound</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Тривога Sound</source>
+        <translation>Тривога Sound</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>GPS</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>GPS</source>
+        <translation>GPS</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Wi Fi</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Wi Fi</source>
+        <translation>Wi Fi</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>17:54</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>17:54</source>
+        <translation>17:54</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Режим радару</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Режим радару</source>
+        <translation>Режим радару</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Режим карти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Режим карти</source>
+        <translation>Режим карти</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Меню</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Меню</source>
+        <translation>Меню</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Jammer</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Jammer</source>
+        <translation>Jammer</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>00:00:00</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>00:00:00</source>
+        <translation>00:00:00</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>ON</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>ON</source>
+        <translation>ON</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>OFF</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>OFF</source>
+        <translation>OFF</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>K1</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>K1</source>
+        <translation>K1</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>K2</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>K2</source>
+        <translation>K2</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>K3</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>K3</source>
+        <translation>K3</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Радіус дії радару</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Радіус дії радару</source>
+        <translation>Радіус дії радару</translation>
     </message>
     <message>
-      <location filename="..\ui\main_window.ui" line="0" />
-      <source>Зберегти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/main_window.ui" line="0"/>
+        <source>Зберегти</source>
+        <translation>Зберегти</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>MapSettingsDialog</name>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Налаштування власної карти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Налаштування власної карти</source>
+        <translation>Налаштування власної карти</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Обрати зображення</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Обрати зображення</source>
+        <translation>Обрати зображення</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Встановити центр</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Встановити центр</source>
+        <translation>Встановити центр</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Масштаб (%)</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Масштаб (%)</source>
+        <translation>Масштаб (%)</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>-</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>-</source>
+        <translation>-</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>+</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>+</source>
+        <translation>+</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Радіус кола (м)</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Радіус кола (м)</source>
+        <translation>Радіус кола (м)</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Кут повороту</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Кут повороту</source>
+        <translation>Кут повороту</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Зберегти</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Зберегти</source>
+        <translation>Зберегти</translation>
     </message>
     <message>
-      <location filename="..\ui\set_map_dialog.ui" line="0" />
-      <source>Скасувати</source>
-      <translation type="unfinished" />
+        <location filename="../ui/set_map_dialog.ui" line="0"/>
+        <source>Скасувати</source>
+        <translation>Скасувати</translation>
     </message>
-  </context>
+</context>
 </TS>

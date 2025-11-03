55526aa878b76c9acd4422b03d6d93af7013df49
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-03 18:16:28 +0200

    Added MessageBox for map loading error

--- app/widgets/main_window.py
+++ app/widgets/main_window.py
@@ -592,6 +592,7 @@ class MainWindow(QMainWindow):
             self.scale_map()
 
         else:
+            QMessageBox.warning(None, "Помилка", f"Не вдалося завантажити карту.")
             print("Не вдалося завантажити карту.")
 
     def scale_map(self):

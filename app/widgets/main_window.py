41cd5fd688d1f27bacd4d52efc12cc6d9e252f3c
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-11-25 18:50:00 +0200

    Disconnected KeyboardService

--- app/widgets/main_window.py
+++ app/widgets/main_window.py
@@ -47,7 +47,8 @@ from app.services.api_server import ApiServer
 from app.services.map_service import MapService, MapTypes
 from app.utils.test_data_provider import TestDataProvider
 from app.services.settings_service import SettingsService
-#from app.services.keyboard_service import KeyboardService
+
+# from app.services.keyboard_service import KeyboardService
 from app.widgets.set_map_dialog import SetMapDialog
 from app.widgets.autosize_window import make_scalable
 from app.widgets.record_status_widget import RecordingStatusWidget
@@ -72,9 +73,7 @@ class MainWindow(QMainWindow):
     _sig_start_recording = pyqtSignal(str)
     _sig_stop_recording = pyqtSignal()
 
-    def __init__(
-        self, settings: SettingsService, keyboard, parent=None
-    ):
+    def __init__(self, settings: SettingsService, keyboard, parent=None):
         super().__init__(parent)
         self.settings_service = settings
         self.keyboard_service = keyboard

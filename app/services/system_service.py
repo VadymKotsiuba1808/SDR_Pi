2e29e2a8927badde5f3a07cf7400f05a5e871d7c
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-03 10:17:45 +0200

    Created SystemService

---
+++ app/services/system_service.py
@@ -0,0 +1,12 @@
+import platform
+import sys
+import os
+
+
+class SystemService:
+    def __init__(self):
+        self.os = platform.system()  # "Windows", "Linux", "Darwin"
+        self.python_version = sys.version
+        self.is_windows = self.os == "Windows"
+        self.is_linux = self.os == "Linux"
+        self.app_dir = os.path.dirname(os.path.abspath(__file__))

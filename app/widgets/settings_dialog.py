e73690836c9a55c71e791081202c477671717143
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-12 21:46:28 +0200

    Added SettingsDialog logic

---
+++ app/widgets/settings_dialog.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from PyQt6.QtWidgets import QDialog
+from PyQt6.QtCore import Qt, QEvent
+from PyQt6 import uic
+
+from app.protocols import SettingsDialogSettings
+from app.ui.ui_settings_dialog import Ui_SettingsDialog
+
+
+@dataclass
+class SettingsData:
+    """Клас для зберігання налаштувань діалогу"""
+
+    max_radius_km: float = 1.0
+    gps_interval_min: int = 5
+    main_relay: str = "K1"
+
+
+class SettingsDialog(QDialog):
+    """
+    Сторінка налаштувань.
+    Логіка відображення та зміни конфігурації системи (радіус дії, гучність, системні параметри).
+    """
+
+    def __init__(self, settings_service: SettingsDialogSettings = None, parent=None):
+        super().__init__(parent)
+
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.Dialog)
+
+        self.settings_service = settings_service
+
+        self._load_ui()
+        self._adjust_fields()
+        self._connect_handlers()
+
+    def changeEvent(self, event):
+        if event.type() == QEvent.Type.LanguageChange:
+            if self.settings_service.compiled_ui_using_enabled:
+                print("Зміна мови, оновлюю UI...")
+                self.ui.retranslateUi(self)
+        else:
+            super().changeEvent(event)
+
+    def _load_ui(self):
+        if self.settings_service.compiled_ui_using_enabled:
+            self.ui = Ui_SettingsDialog()
+            self.ui.setupUi(self)
+        else:
+            uic.loadUi("app/ui/settings_dialog.ui", self)
+            self.ui = self
+
+    def _adjust_fields(self):
+        s = self.settings_service
+
+        self.ui.inpMaxRadius.setValue(s.radar_max_radius)
+        self.ui.inpGpsInterval.setValue(s.gps_interval_min)
+
+        relay_val = s.main_relay
+        index = self.ui.cmbRelay.findText(relay_val)
+        if index >= 0:
+            self.ui.cmbRelay.setCurrentIndex(index)
+
+    def _connect_handlers(self):
+        self.ui.btnSave.clicked.connect(self._handle_save)
+        self.ui.btnCancel.clicked.connect(self.reject)
+
+    def _handle_save(self):
+        self.new_settings = SettingsData(
+            max_radius_km=self.ui.inpMaxRadius.value(),
+            gps_interval_min=self.ui.inpGpsInterval.value(),
+            main_relay=self.ui.cmbRelay.currentText(),
+        )
+
+        print(f"[SettingsDialog] Збережено: {self.new_settings}")
+        self.accept()
+
+    def get_settings(self) -> SettingsData:
+        return getattr(self, "new_settings", {})

4fbc916dd5a39de95a0e851d0b72ac91d5d03d8d
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-12-31 22:02:46 +0200

    Added jammer fields to settings

--- app/protocols.py
+++ app/protocols.py
@@ -16,11 +16,6 @@ class MapServiceSettings(Protocol):
     tile_divider_enabled: bool
 
 
-class ApiServerSettings(Protocol):
-    host: str
-    port: int
-
-
 class LoginDialogSettings(Protocol):
     role: str
     owner_password_hash: str
@@ -70,7 +65,7 @@ class SettingsDialogSettings(Protocol):
     lang_code: str
     radar_max_radius: float
     gps_interval_s: float
-    main_relay: str
+    main_relays: str
     remember_me: bool
 
 

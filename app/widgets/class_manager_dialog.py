4c7882e82fc530fe985d9b4b188041fcc9dcc626
Bohdan Miskov <bohdanmiskov07@gmail.com>
2026-01-02 22:50:31 +0200

    Connected ClassManagerDialog to NetworkService

--- app/widgets/class_manager_dialog.py
+++ app/widgets/class_manager_dialog.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from PyQt6.QtWidgets import (
     QDialog,
@@ -12,14 +12,15 @@ from PyQt6.QtCore import Qt, QEvent, QTranslator, QCoreApplication
 from app.ui.ui_class_manager_dialog import Ui_ClassManagerDialog
 from app.widgets.keyboard_widget import KeyboardWidget
 from app.services.keyboard_service import KeyboardService
-from app.services.database_service import DatabaseService
+from app.services.pi_network_service import PiNetworkService
+from app.models.object_class import ObjectClass
 from app.protocols import ClassManagerDialogSettings
 
 
 class ClassManagerDialog(QDialog):
     def __init__(
         self,
-        db_service: DatabaseService,
+        network_service: PiNetworkService,
         settings_service: ClassManagerDialogSettings,
         keyboard_service: KeyboardService,
         parent: Optional[QWidget] = None,
@@ -27,7 +28,7 @@ class ClassManagerDialog(QDialog):
         super().__init__(parent)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.Dialog)
 
-        self.db_service = db_service
+        self.network_service = network_service
         self.settings_service = settings_service
         self.keyboard_service = keyboard_service
 
@@ -58,6 +59,7 @@ class ClassManagerDialog(QDialog):
         self.lang_widget = KeyboardWidget(
             self.settings_service, self.keyboard_service, parent=self
         )
+        self.cached_classes: List[ObjectClass] = []
 
     def _adjust_fields(self) -> None:
         self.ui.keyboardLayout.addWidget(self.lang_widget)
@@ -69,6 +71,12 @@ class ClassManagerDialog(QDialog):
         self.ui.btnClose.clicked.connect(self.accept)
         self.ui.lstClasses.itemClicked.connect(self._on_item_clicked)
 
+        self.network_service.db_operation_status.connect(self._handle_db_status)
+        self.network_service.db_classes_received.connect(self._populate_list)
+        self.network_service.db_class_added.connect(self.add_cache_class)
+        self.network_service.db_class_renamed.connect(self.update_cache_class)
+        self.network_service.db_class_deleted.connect(self.delete_cache_class)
+
         if hasattr(self.ui, "btnClearSelection"):
             self.ui.btnClearSelection.clicked.connect(self._clear_selection)
 
@@ -79,12 +87,36 @@ class ClassManagerDialog(QDialog):
         QCoreApplication.removeTranslator(self.translator)
 
     def _refresh_list(self) -> None:
+
+        self.network_service.request_db_classes()
+        print(f"[ClassManager] Loaded classes.")
+
+    def _handle_db_status(self, op_type: str, success: bool, msg: str) -> None:
+        if success:
+            return
+
+        titles = {
+            "add_class": "Помилка створення класу",
+            "rename_class": "Помилка перейменування",
+            "delete_class": "Помилка видалення",
+            "get_classes": "Помилка завантаження списку",
+            "unknown": "Системна помилка",
+        }
+
+        title = titles.get(op_type, "Помилка операції")
+
+        if op_type == "delete_class":
+            msg += "\nМожливо ваш клас використовується певними об'єктами"
+
+        QMessageBox.critical(self, title, msg)
+
+    def _populate_list(self, classes: Optional[List[ObjectClass]] = None):
+        if classes:
+            self.cached_classes = classes
+
         self.ui.lstClasses.clear()
-        classes = self.db_service.get_all_classes()
 
-        print(f"[ClassManager] Loaded {len(classes)} classes.")
-
-        for c in classes:
+        for c in self.cached_classes:
             item = QListWidgetItem(c.name)
             item.setData(Qt.ItemDataRole.UserRole, c.id)
             self.ui.lstClasses.addItem(item)
@@ -116,20 +148,21 @@ class ClassManagerDialog(QDialog):
                 return
 
             print(f"[ClassManager] Renaming class '{old_name}' to '{text}'...")
+            index = next(
+                (i for i, x in enumerate(self.cached_classes) if x.name == old_name),
+                None,
+            )
+            if index is not None:
+                old_class = self.cached_classes[index]
+                new_class = ObjectClass(name=text)
 
-            if self.db_service.rename_class(old_name, text):
-                self._refresh_list()
-            else:
-                print(f"[ClassManager] Error: Failed to rename '{old_name}'.")
-                QMessageBox.warning(self, "Помилка", "Помилка перейменування.")
+                self.network_service.request_db_rename_class(old_class, new_class)
+
         else:
             print(f"[ClassManager] Adding new class '{text}'...")
 
-            if self.db_service.add_class(text):
-                self._refresh_list()
-            else:
-                print(f"[ClassManager] Error: Failed to add '{text}'.")
-                QMessageBox.warning(self, "Помилка", "Такий клас вже існує.")
+            new_class = ObjectClass(name=text)
+            self.network_service.request_db_add_class(new_class)
 
     def _delete_class(self) -> None:
         item = self.ui.lstClasses.currentItem()
@@ -141,18 +174,6 @@ class ClassManagerDialog(QDialog):
         class_id_data = item.data(Qt.ItemDataRole.UserRole)
         class_id = int(class_id_data) if class_id_data is not None else -1
 
-        if self.db_service.is_class_used(class_id):
-            print(
-                f"[ClassManager] Blocked deletion: Class '{class_name}' (ID: {class_id}) is in use."
-            )
-            QMessageBox.critical(
-                self,
-                "Неможливо видалити",
-                f"Клас '{class_name}' використовується одним або кількома об'єктами.\n"
-                "Спочатку видаліть або змініть об'єкти цього класу.",
-            )
-            return
-
         res = QMessageBox.question(
             self,
             "Видалення",
@@ -163,5 +184,25 @@ class ClassManagerDialog(QDialog):
         if res == QMessageBox.StandardButton.Yes:
             print(f"[ClassManager] Deleting class '{class_name}' (ID: {class_id})...")
 
-            self.db_service.delete_class(class_id)
-            self._refresh_list()
+            self.network_service.request_db_delete_class(class_id)
+
+    def add_cache_class(self, class_obj: ObjectClass):
+        self.cached_classes.append(class_obj)
+        self._populate_list()
+
+    def update_cache_class(self, new_class_obj: ObjectClass):
+        index = next(
+            (i for i, x in enumerate(self.cached_classes) if x.id == new_class_obj.id),
+            None,
+        )
+
+        if index is not None:
+            self.cached_classes[index] = new_class_obj
+            self._populate_list()
+
+    def delete_cache_class(self, id: int):
+        index = next((i for i, x in enumerate(self.cached_classes) if x.id == id), None)
+
+        if index is not None:
+            self.cached_classes.pop(index)
+            self._populate_list()

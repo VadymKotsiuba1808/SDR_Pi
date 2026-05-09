f9daf21a94ff5991cb858fd90db6931e0efd0aa8
Bohdan Miskov <bohdanmiskov07@gmail.com>
2026-05-09 14:20:07 +0300

    Delete server role ability for PiServerService

--- app/services/settings_service.py
+++ app/services/settings_service.py
@@ -26,7 +26,6 @@ class SettingsService(QObject):
     # Pinetwork
     pi_target_ip: str
     pi_target_port: int
-    pi_is_receiver: bool
     # Maps
     radar_radius_km: float
     radar_max_radius_km: float
@@ -57,7 +56,6 @@ class SettingsService(QObject):
         # Pinetwork
         "pi_target_ip": ("pinetwork", str, "0.0.0.0"),
         "pi_target_port": ("pinetwork", int, 6000),
-        "pi_is_receiver": ("pinetwork", bool, True),
         # Maps
         "radar_radius_km": ("maps", float, 100),
         "radar_max_radius_km": ("maps", float, 200),

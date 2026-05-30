81b3ece42f5e7d63f3f75c19e574903b21858920
Bohdan Miskov <bohdanmiskov07@gmail.com>
2026-05-30 23:25:29 +0300

    Format tests

--- tests/widgets/test_change_pwd_dialog.py
+++ tests/widgets/test_change_pwd_dialog.py
@@ -50,12 +50,12 @@ def test_password_mismatch(change_pwd_dialog, qtbot):
     """Тест помилки при незбігу паролів."""
     change_pwd_dialog.ui.passwordLineEdit.clear()
     change_pwd_dialog.ui.confirmPasswordLineEdit.clear()
-    
+
     qtbot.keyClicks(change_pwd_dialog.ui.passwordLineEdit, "StrongPass123")
     qtbot.keyClicks(change_pwd_dialog.ui.confirmPasswordLineEdit, "DifferentPass123")
-    
+
     qtbot.mouseClick(change_pwd_dialog.ui.saveButton, pytest.importorskip("PyQt6.QtCore").Qt.MouseButton.LeftButton)
-    
+
     # Використовуємо isHidden() замість isVisible(), бо діалог може бути не показаний реально
     assert not change_pwd_dialog.ui.errorWidget_2.isHidden()
 
@@ -64,13 +64,13 @@ def test_password_validation_fail(change_pwd_dialog, qtbot):
     """Тест провалу валідації (дуже короткий пароль)."""
     change_pwd_dialog.ui.passwordLineEdit.clear()
     change_pwd_dialog.ui.confirmPasswordLineEdit.clear()
-    
+
     # Пароль занадто короткий (менше 4 символів)
     qtbot.keyClicks(change_pwd_dialog.ui.passwordLineEdit, "123")
     qtbot.keyClicks(change_pwd_dialog.ui.confirmPasswordLineEdit, "123")
-    
+
     qtbot.mouseClick(change_pwd_dialog.ui.saveButton, pytest.importorskip("PyQt6.QtCore").Qt.MouseButton.LeftButton)
-    
+
     assert not change_pwd_dialog.ui.errorWidget_1.isHidden()
 
 
@@ -78,14 +78,14 @@ def test_successful_password_change(change_pwd_dialog, mock_settings, qtbot):
     """Тест успішної зміни пароля."""
     change_pwd_dialog.ui.passwordLineEdit.clear()
     change_pwd_dialog.ui.confirmPasswordLineEdit.clear()
-    
+
     password = "ValidPassword123!"
     qtbot.keyClicks(change_pwd_dialog.ui.passwordLineEdit, password)
     qtbot.keyClicks(change_pwd_dialog.ui.confirmPasswordLineEdit, password)
-    
+
     with qtbot.waitSignal(change_pwd_dialog.finished, timeout=1000) as blocker:
         qtbot.mouseClick(change_pwd_dialog.ui.saveButton, pytest.importorskip("PyQt6.QtCore").Qt.MouseButton.LeftButton)
-    
+
     assert blocker.args == [QDialog.DialogCode.Accepted]
     assert mock_settings.owner_password_hash.startswith("$2b$")
 
@@ -96,7 +96,7 @@ def test_toggle_visibility(change_pwd_dialog, qtbot):
     assert change_pwd_dialog.ui.passwordLineEdit.echoMode() == QLineEdit.EchoMode.Password
     qtbot.mouseClick(change_pwd_dialog.ui.passwordHideBtn, pytest.importorskip("PyQt6.QtCore").Qt.MouseButton.LeftButton)
     assert change_pwd_dialog.ui.passwordLineEdit.echoMode() == QLineEdit.EchoMode.Normal
-    
+
     # Друге поле
     assert change_pwd_dialog.ui.confirmPasswordLineEdit.echoMode() == QLineEdit.EchoMode.Password
     qtbot.mouseClick(change_pwd_dialog.ui.confirmPasswordHideBtn, pytest.importorskip("PyQt6.QtCore").Qt.MouseButton.LeftButton)
